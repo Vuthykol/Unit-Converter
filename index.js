ba799d65b35e242b9d17637be720d9ef509fb733
Song Vuthykol <vuthykolsong0@gmail.com>
2026-01-06 12:08:45 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let inputEl = document.getElementById("input-text")
+let btnEl = document.getElementById("btn-el")
+let convertLength = document.getElementById('converter1')
+let convertVolume = document.getElementById('converter2')
+let convertMass = document.getElementById('converter3')
+
+
+
